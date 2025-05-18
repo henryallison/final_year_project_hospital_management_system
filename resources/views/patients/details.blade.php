8d947007f6d203fd07653a6991a43efb8c6bbcb7
henryallison <164251136+henryallison@users.noreply.github.com>
2025-05-18 21:57:59 +0200

    Create details.blade.php

---
+++ resources/views/patients/details.blade.php
@@ -0,0 +1,115 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+            <h4 class="mb-3">Basic Information</h4>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Name</th>
+                        <td>{{ $patient->name }}</td>
+                    </tr>
+                    <tr>
+                        <th>Date of Birth</th>
+                        <td>{{ $patient->date_of_birth->format('Y-m-d') }}</td>
+                    </tr>
+                    <tr>
+                        <th>Gender</th>
+                        <td>{{ ucfirst($patient->gender) }}</td>
+                    </tr>
+                    <tr>
+                        <th>Contact Number</th>
+                        <td>{{ $decryptedData['contact_number'] }}</td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td>{{ $decryptedData['address'] }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <h4 class="mb-3">Medical Information</h4>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Blood Type</th>
+                        <td>{{ $decryptedData['blood_type'] }}</td>
+                    </tr>
+                    <tr>
+                        <th>Height</th>
+                        <td>{{ $decryptedData['height'] }} cm</td>
+                    </tr>
+                    <tr>
+                        <th>Weight</th>
+                        <td>{{ $decryptedData['weight'] }} kg</td>
+                    </tr>
+                    <tr>
+                        <th>Chronic Conditions</th>
+                        <td>{{ $decryptedData['chronic_conditions'] }}</td>
+                    </tr>
+                    <tr>
+                        <th>Family Medical History</th>
+                        <td>{{ $decryptedData['family_medical_history'] }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <h4 class="mb-3">Medical Details</h4>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Medical History</h5>
+                    <p class="card-text">{{ $patient->medical_history }}</p>
+                </div>
+            </div>
+
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">Allergies</h5>
+                    <p class="card-text">{{ $patient->allergies }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <h4 class="mb-3">Treatment Information</h4>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Current Medications</h5>
+                    <p class="card-text">{{ $patient->current_medications }}</p>
+                </div>
+            </div>
+
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">Admission Details</h5>
+                    <p><strong>Admission Date:</strong> {{ $patient->admission_date->format('Y-m-d') }}</p>
+                    <p><strong>Discharge Date:</strong> {{ $patient->discharge_date ? $patient->discharge_date->format('Y-m-d') : 'N/A' }}</p>
+                    <p><strong>Status:</strong> {{ ucfirst($patient->status) }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <h4 class="mb-3">Assigned Staff</h4>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Doctor</th>
+                        <td>{{ $patient->doctor ? $patient->doctor->name : 'Unassigned' }}</td>
+                    </tr>
+                    <tr>
+                        <th>Nurse</th>
+                        <td>{{ $patient->nurse ? $patient->nurse->name : 'Unassigned' }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
